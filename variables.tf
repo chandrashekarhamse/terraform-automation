941eb9d41964ef6337f537dd16b762d3df6f9e5d
chandrashekarhamse <50998410+chandrashekarhamse@users.noreply.github.com>
2025-09-25 19:41:24 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "ami_id" {
+    type = string
+}
+
+variable "instance_type_master" {
+    type = string
+}
+
+variable "instance_type_worker" {
+    type = string
+}
+
+variable "instance_name_master" {
+    type = string
+}
+
+variable "instance_name_worker" {
+    type = string
+}
+
+variable "key_name" {
+    type = string
+}
+
+variable "k8s_master_sg" {
+    type = string
+}
+
+variable "k8s_worker_sg" {
+    type = string 
+}
+
+variable "vpc_id" {
+    type = string
+}
